129bcaf883dd64b291f698589f13ae3f60194a52
Dmitry Bochenkov <pishisudapridurok@yandex.ru>
2026-07-05 23:53:39 +0300

    Rearrange main keyboard

--- backend/app/bot/telegram/keyboards/main.py
+++ backend/app/bot/telegram/keyboards/main.py
@@ -3,10 +3,12 @@ from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
 def main_keyboard(is_admin: bool = False) -> ReplyKeyboardMarkup:
     rows = [
-        [KeyboardButton(text="Расписание турниров")],
+        [
+            KeyboardButton(text="Расписание турниров"),
+            KeyboardButton(text="Как нас найти"),
+        ],
         [KeyboardButton(text="Записаться"), KeyboardButton(text="Отменить запись")],
         [KeyboardButton(text="Рейтинг"), KeyboardButton(text="Твой профиль")],
-        [KeyboardButton(text="Как нас найти")],
     ]
     if is_admin:
         rows.append([KeyboardButton(text="Админ-панель")])
